81299e24b6e0fc502343284bacb9575e2fb814e7
vcshing <vcshing2005@hotmail.com>
2018-01-30 21:47:19 +0800

    asd

--- www/commonjs/global/langArr.js
+++ www/commonjs/global/langArr.js
@@ -6,12 +6,12 @@ var admobid = {};
 
 
 appConfigArr = {
-    "appName": "投票",
-    "appDescription": "投票",
+    "appName": "TeleGroup",
+    "appDescription": "TeleGroup",
     "androidAppLink": "https://play.google.com/store/apps/details?id=com.skyexplorer.telegroup",
-    "androidAdmobBannerID": "ca-app-pub-3715336230214756/7051579428",
-    "androidAdmobInterstitialID": "ca-app-pub-3715336230214756/9103027690",
-    "androidAdmobBonusID": "ca-app-pub-3715336230214756/9768763179",
+    "androidAdmobBannerID": "ca-app-pub-3715336230214756/4301873563",
+    "androidAdmobInterstitialID": "ca-app-pub-3715336230214756/4554699592",
+    "androidAdmobBonusID": "ca-app-pub-3715336230214756/2741379888",
     "isTesting": false
 }
 appArr = {
